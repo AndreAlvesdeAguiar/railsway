daceb424a212c0b9aeff3c0a1f14993dbd3b37be
Andre Alves de Aguiar <aguiar1001@hotmail.com>
2023-04-24 15:39:58 -0300

    adicionado chart.js

--- app/controllers/dados_controller.rb
+++ app/controllers/dados_controller.rb
@@ -1,4 +1,4 @@
-class DadosController < ApplicationController
+class DadosController < ActionController::Base
   def index
     codigo_serie = params[:codigo_serie]
     dados_json = $redis.get(codigo_serie)
@@ -11,7 +11,44 @@ class DadosController < ApplicationController
 
     render json: JSON.parse(dados_json)
   end
+
+  def grafico 
+    # Carrega os dados necessários para o gráfico em um formato compreensível pelo Chart.js
+    codigo_serie = params[:codigo_serie]
+    dados_json = $redis.get(codigo_serie)
+    if dados_json.nil?
+      dados = ApiService.busca_dados(codigo_serie)
+      dados_json = dados.to_json
+      $redis.set(codigo_serie, dados_json)
+    end
+    dados = JSON.parse(dados_json)
+
+  
+    labels = []
+    valores = []
+    dados.each do |dado|
+      labels << dado['data']
+      valores << dado['valor']
+    end
+  
+    @dados_grafico = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Valores',
+          data: valores,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }
+      ]
+    }
+  
+    render 'dados/grafico'
+  end
+
 end
+
 
 
   #CONTROLLER EXCLUSIVO DO REDIS (VISUAL)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
-    get 'dados/:codigo_serie', to: 'dados#index'
+  get 'dados/:codigo_serie', to: 'dados#index'
+  get 'dados/:codigo_serie/grafico', to: 'dados#grafico'
 end
